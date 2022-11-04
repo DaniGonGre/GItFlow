1c6c1daee10f39c54ab9845c3b3c8761df2bb85c
DaniGonGre <dgonzalezgregores@danielcastelao.org>
2022-11-04 12:49:28 +0100

    Añadido nombre

--- src/com/luis/Main.java
+++ src/com/luis/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Soy Luis, que pasa??!");
+        System.out.println("Soy Dani");
     }
 }
